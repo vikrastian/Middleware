18ee5724cce893c004727f06df58b05adc58c7bc
vikrastian <157480368+vikrastian@users.noreply.github.com>
2024-07-01 15:15:43 +0200

    Commit

--- Middleware/Program.cs
+++ Middleware/Program.cs
@@ -8,21 +8,22 @@ var app = builder.Build();
 // Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("Welcome from ASP.NET Core App!");
+    await context.Response.WriteAsync("MIddleware 1: \"Welcome from ASP.NET Core App!\"");
     await next(context);
 });
 
 // Middleware 2
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("\n\n");
+    await context.Response.WriteAsync("\n\nMiddleware 2: \"I am middleware 2\"\n\n");
     await next(context);
+    await context.Response.WriteAsync("\n\nMiddleware 2: \"I wrote this on my way back, after visiting middleware 3\"");
 });
 
 // Middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("This is my first ASP.NET Core APP!");
+    await context.Response.WriteAsync("Middleware 3: \"This is my first ASP.NET Core APP!\"");
 });
 
 app.Run();
